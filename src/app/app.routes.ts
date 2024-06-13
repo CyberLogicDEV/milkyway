7b84a45942f9681029384a8643b544144ea33b34
CyberLogic <office@wemake.website>
2024-06-13 15:20:10 +0200

    routing for main ui

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'game',
+        pathMatch: 'full',
+    },
+    {
+        path: 'game',
+        loadChildren: (): any => import('./pages/client-view/dashboard/dashboard.module').then(m => m.DashboardModule),
+    },
+];
