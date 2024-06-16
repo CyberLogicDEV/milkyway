b2ea06bbd822027f96db37c180428e387622a14c
CyberLogic <office@wemake.website>
2024-06-16 19:59:54 +0200

    config for unit tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'milkyway' title`, () => {
+  it(`should have the 'StarWars Galaxy Adventure' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('milkyway');
+    expect(app.title).toEqual('StarWars Galaxy Adventure');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, milkyway');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, StarWars Galaxy Adventure');
   });
 });
